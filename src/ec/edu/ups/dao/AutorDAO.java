373adf686ea1b619a8f006ea796811c0d487274b
OrlandoR54 <OrlandoR54@users.noreply.github.com>
2020-06-02 11:33:57 -0500

    Create AutorDAO.java

---
+++ src/ec/edu/ups/dao/AutorDAO.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package ec.edu.ups.dao;
+
+import ec.edu.ups.entidades.Autor;
+
+/**
+ * @author Orlando Real
+ *
+ */
+public interface AutorDAO extends GenericDAO<Autor, String>{
+
+}
