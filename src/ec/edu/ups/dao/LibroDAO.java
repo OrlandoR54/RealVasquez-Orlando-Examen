80be9d94352a76330f8789c751015fb76c858556
OrlandoR54 <OrlandoR54@users.noreply.github.com>
2020-06-02 11:34:39 -0500

    Create LibroDAO.java

---
+++ src/ec/edu/ups/dao/LibroDAO.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package ec.edu.ups.dao;
+
+import ec.edu.ups.entidades.Libro;
+
+/**
+ * @author Orlando Real
+ *
+ */
+public interface LibroDAO extends GenericDAO<Libro, String>{
+
+}
