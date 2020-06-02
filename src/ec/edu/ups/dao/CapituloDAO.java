a6d0e777637b891d2db0f8076cda905fa412f828
OrlandoR54 <OrlandoR54@users.noreply.github.com>
2020-06-02 11:33:55 -0500

    Create CapituloDAO.java

---
+++ src/ec/edu/ups/dao/CapituloDAO.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package ec.edu.ups.dao;
+
+import ec.edu.ups.entidades.Capitulo;
+
+/**
+ * @author Orlando Real
+ *
+ */
+public interface CapituloDAO extends GenericDAO<Capitulo, String>{
+
+}
